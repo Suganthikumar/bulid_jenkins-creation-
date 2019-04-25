6212f7c6dd4dc9fd40a0f1baa99be5451b5c2b09
Suganthikumar <49432873+Suganthikumar@users.noreply.github.com>
2019-04-25 10:36:09 +0530

    Add files via upload

---
+++ basic.php
@@ -0,0 +1,174 @@
+<?php /* echo "helloworld suganthi"; */
+?>
+ 
+ 
+<?php
+/* $n=8;
+if($n%2!=0)
+{
+	echo "A";
+}
+elseif($n%2==0 and 2<=$n and $n<=5)
+{
+    echo "B";
+}
+elseif($n%2==0 and 6<=$n and $n<=20)
+{
+     echo "C";
+}
+elseif($n%2==0 and $n>=20)
+{
+     echo "D";
+} */
+?>	
+
+
+<?php
+/* $n=2;
+$s=3;
+$v=1;
+if($n==$s and $s==$v and $v==$n)
+{
+	$p=$n+$s+$v;
+	$o=$p*3;
+	echo "The number is :$o";
+}
+elseif($n==$s or $s==$v or $v==$n)
+{
+	$i=$n+$s+$v;
+	$q=$i*2;
+	echo "The number is :$q";
+}
+else
+{
+	$m=$n+$s+$v;
+	echo "The number is :$m";
+} */
+?>
+
+<?php
+/* $name="Suganthi";
+$nam=strlen($name);
+if($nam>6)
+{
+ echo "$nam";
+}
+else
+{
+  echo "Error Message";
+}   */
+?>
+
+<?php
+/* echo substr_count("Suganthissssssss","S"); */
+?>
+
+<?php
+/* $marks=array(100,45,89,23);
+sort($marks);
+$sugan=count($marks);
+for($s=0;$s<$sugan;$s++)
+{
+	echo $marks[$s];
+} */	
+?>
+
+<?php
+/* $marks=array(10,45,81,101);
+$ma=min($marks);
+echo $ma;
+$rk=max($marks);
+echo $rk; */
+?>
+
+<?php
+/* $marks=array(100,45,89,23);
+$mark=array_reverse($marks);
+$sugan=count($mark);
+for($s=0;$s<$sugan;$s++)
+{
+	echo $mark[$s];
+} */	
+?>
+
+<?php
+/* $d='2001-11-01';
+$b=date('w',strtotime($d));
+echo $b;
+if($b==0)
+{
+	echo "Sunday";
+}
+if($b==1)
+{
+	echo "Monday";
+}
+if($b==2)
+{
+	echo "Tuesday";
+}
+if($b==3)
+{
+	echo "Wednesday";
+}
+if($b==4)
+{
+	echo "Thursday";
+}
+if($b==5)
+{
+	echo "Friday";
+}
+if($b==6)
+{
+	echo "Saturday";
+}
+switch($b)
+{
+	case 0:echo "Those who sleeps in class will neer attain Moksha:P";
+	        break;
+	case 1:echo "Marriages are made in heaven";
+            break;
+    case 2:echo "Highest scores earn the least salaries";			
+			break;
+	case 3:echo "Those who question too much will never get answers";
+            break;
+    case 4:echo "Neat presentation earns less marks";
+            break;
+    case 5:echo "Those who asks doubts without listening will not get food and water";
+            break;
+    case 6:echo "Sincerity fails";
+			break;
+	default:echo "Sssss";		
+} */
+?>
+
+<?php
+/* $s=[1,2,3];
+foreach($s as $i)
+{ 
+    echo $i;
+	echo "<br>";
+} */
+?>
+
+<?php
+/* $d=["name"=>"SSN", "year"=>2019];
+foreach($d as $i=>$j)
+{
+	echo $i;
+	echo "<br>";
+	echo $j;
+	echo "<br>";
+	echo $d["name"];
+	echo $d["year"];
+} */
+?>	
+<?php
+/* date_default_timezone_set('Asia/Kolkata');
+$dt2=date("Y-m-d H:i:s");
+echo $dt2;
+echo "<br>";
+$dt2=date("Y-m-d h:i:s a");
+echo $dt2; */
+?>
